1f1dce5f463514a9b236018ed0ae410f52e414fa
Claudio Bruno <cdbruno10@hotmail.com>
2023-05-03 12:13:54 -0300

    casi termina el js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // capturo a todas las clases al mimo tiempo
 const question = document.getElementsByClassName("question");
+const arrow = document.getElementsByClassName("arrow");
 // capturo a los id de otra forma, individual para probar
 const paragraph1 = document.getElementById("paragraph1");
 const paragraph2 = document.getElementById("paragraph2");
@@ -11,44 +12,55 @@ for (let i = 0; i < question.length; i++) {
   question[i].addEventListener("click", questionNum);
 }
 
-function questionNum(event) {
+function questionNum(e) {
   console.log(event.target.id);
-  //event.preventDefault();
+  question[0].style.fontWeight = "400";
+  question[1].style.fontWeight = "400";
+  question[2].style.fontWeight = "400";
+  question[3].style.fontWeight = "400";
+  question[4].style.fontWeight = "400";
+  arrow[0].style.rotate = "0deg";
+  arrow[1].style.rotate = "0deg";
+  arrow[2].style.rotate = "0deg";
+  arrow[3].style.rotate = "0deg";
+  arrow[4].style.rotate = "0deg";
+  paragraph1.style.display = "none";
+  paragraph2.style.display = "none";
+  paragraph3.style.display = "none";
+  paragraph4.style.display = "none";
+  paragraph5.style.display = "none";
   switch (event.target.id) {
     case "question1":
       paragraph1.style.display = "block";
-      paragraph2.style.display = "none";
-      paragraph3.style.display = "none";
-      paragraph4.style.display = "none";
-      paragraph5.style.display = "none";
+      question[0].style.fontWeight = "bold";
+      arrow[0].style.rotate = "180deg";
+
       break;
     case "question2":
-      paragraph1.style.display = "none";
       paragraph2.style.display = "block";
-      paragraph3.style.display = "none";
-      paragraph4.style.display = "none";
-      paragraph5.style.display = "none";
+      question[1].style.fontWeight = "bold";
+      arrow[1].style.rotate = "180deg";
+
       break;
     case "question3":
-      paragraph1.style.display = "none";
-      paragraph2.style.display = "none";
       paragraph3.style.display = "block";
-      paragraph4.style.display = "none";
-      paragraph5.style.display = "none";
+      question[2].style.fontWeight = "bold";
+      arrow[2].style.rotate = "180deg";
+
       break;
     case "question4":
-      paragraph1.style.display = "none";
-      paragraph2.style.display = "none";
-      paragraph3.style.display = "none";
       paragraph4.style.display = "block";
-      paragraph5.style.display = "none";
+      question[3].style.fontWeight = "bold";
+      arrow[3].style.rotate = "180deg";
+
       break;
     case "question5":
-      paragraph1.style.display = "none";
-      paragraph2.style.display = "none";
-      paragraph3.style.display = "none";
-      paragraph4.style.display = "none";
       paragraph5.style.display = "block";
+      question[4].style.fontWeight = "bold";
+      arrow[4].style.rotate = "180deg";
+
       break;
+    default:
+      return;
   }
 }
